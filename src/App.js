89c81a9cdec40d873176a9a802b800680cc47035
Mitesu <89779807+Mitesu@users.noreply.github.com>
2022-04-25 01:05:12 +0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Modal from "./modal/Modal";
+import ReactDOM from "react-dom";
+
+function App() {
+    return (
+        <Modal />
+
+    );
+}
+export default App;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Modal from "./modal/Modal";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('regbt')
+);
+
+reportWebVitals();
